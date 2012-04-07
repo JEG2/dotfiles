c59fcb6804cbf35e350d83f4778b58292a3933ea
James Edward Gray II <james@graysoftinc.com>
2012-04-06 22:05:24 -0500

    Tweaking auto completion.

--- emacs.d/settings.el
+++ emacs.d/settings.el
@@ -17,6 +17,15 @@
 
 ;; upgrade normal expansion to Hippie Expand
 (global-set-key (kbd "M-/") 'hippie-expand)
+(setq hippie-expand-try-functions-list
+      '(try-complete-file-name-partially
+        try-complete-file-name
+        try-expand-all-abbrevs
+        try-expand-dabbrev
+        try-expand-dabbrev-all-buffers
+        try-expand-dabbrev-from-kill
+        try-complete-lisp-symbol-partially
+        try-complete-lisp-symbol))
 
 ;; show matching paren, bracket, etc.
 (show-paren-mode 1)

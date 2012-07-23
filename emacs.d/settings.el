271b5ca87652c161b18bff0652a77af1aa24e146
James Edward Gray II <james@graysoftinc.com>
2012-07-23 08:54:06 -0500

    Reordering autocomplete matches.

--- emacs.d/settings.el
+++ emacs.d/settings.el
@@ -18,12 +18,12 @@
 ;; upgrade normal expansion to Hippie Expand
 (global-set-key (kbd "M-/") 'hippie-expand)
 (setq hippie-expand-try-functions-list
-      '(try-complete-file-name-partially
-        try-complete-file-name
-        try-expand-all-abbrevs
+      '(try-expand-all-abbrevs
         try-expand-dabbrev
         try-expand-dabbrev-all-buffers
         try-expand-dabbrev-from-kill
+        try-complete-file-name-partially
+        try-complete-file-name
         try-complete-lisp-symbol-partially
         try-complete-lisp-symbol))
 

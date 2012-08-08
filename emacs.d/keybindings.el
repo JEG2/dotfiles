b9a87ac91f1ce482d8c8b37439941088fdace74c
James Edward Gray II <james@graysoftinc.com>
2012-08-08 10:09:49 -0500

    Adding a previous (other) window command.

--- emacs.d/keybindings.el
+++ emacs.d/keybindings.el
@@ -1,13 +1,16 @@
 ;; window management
-(global-set-key (kbd "C-c f c")       'make-frame-command)
-(global-set-key (kbd "C-c f k")       'delete-frame)
-(global-set-key (kbd "C-c f f")       'other-frame)
+(global-set-key (kbd "C-c f c")     'make-frame-command)
+(global-set-key (kbd "C-c f k")     'delete-frame)
+(global-set-key (kbd "C-c f f")     'other-frame)
 (global-set-key (kbd "C-c -")       'split-window-vertically)
 (global-set-key (kbd "C-c |")       'split-window-horizontally)
 (global-set-key (kbd "C-c <left>")  'windmove-left)
 (global-set-key (kbd "C-c <right>") 'windmove-right)
 (global-set-key (kbd "C-c <up>")    'windmove-up)
 (global-set-key (kbd "C-c <down>")  'windmove-down)
+(global-set-key (kbd "C-x O")       (lambda ()
+                                      (interactive)
+                                      (other-window -1)))
 
 ;; Git
 (global-set-key (kbd "C-c g g") 'magit-status)

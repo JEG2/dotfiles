91e326a0d53a320d5a5904f89034ccef19102e48
James Edward Gray II <james@graysoftinc.com>
2012-08-17 08:46:06 -0500

    Adding multiple toggle commands.

--- emacs.d/functions.el
+++ emacs.d/functions.el
@@ -1,3 +1,5 @@
+(require 'cl)
+
 ;;;;;;;;;;;;;;;;;
 ;;; Functions ;;;
 ;;;;;;;;;;;;;;;;;
@@ -215,4 +217,167 @@
                                                         "\\\\'")
                                    "'"))
                    (goto-char (+ old_point 1)))))))))
-(global-set-key (kbd "C-c s") 'jeg2s-toggle-string-type)
+(global-set-key (kbd "C-c t s") 'jeg2s-toggle-string-type)
+
+(defun jeg2s-toggle-string-and-symbol ()
+  "Toggle between strings and symbols."
+  (interactive)
+  (let ((regex (concat "\\`\\(?:"
+                       "\"\\(?:\\\\\\\\\\|\\\\\.\\|[^\"\\]+\\)*\""
+                       "\\|"
+                       "'\\(?:\\\\\\\\\\|\\\\\.\\|[^'\\]+\\)*'"
+                       "\\|"
+                       ":\\w+"
+                       "\\)\\'")))
+    (while (or (not (region-active-p))
+               (not (or (and (= (point-min) (region-beginning))
+                             (= (point-max) (region-end)))
+                        (string-match regex (buffer-substring-no-properties
+                                             (region-beginning)
+                                             (region-end))))))
+      (call-interactively 'er/expand-region))
+    (let ((matched (buffer-substring-no-properties (region-beginning)
+                                                   (region-end))))
+      (if (string-match regex matched)
+          (cond ((or (string= (substring matched 0 1) "\"")
+                     (string= (substring matched 0 1) "'"))
+                 (call-interactively 'backward-delete-char-untabify)
+                 (let ((old_point (point)))
+                   (insert (concat ":" (substring matched 1 -1)))
+                   (goto-char (+ old_point 1))))
+                ((string= (substring matched 0 1) ":")
+                 (call-interactively 'backward-delete-char-untabify)
+                 (let ((old_point (point)))
+                   (insert (concat "\"" (substring matched 1) "\""))
+                   (goto-char (+ old_point 1)))))))))
+(global-set-key (kbd "C-c t S") 'jeg2s-toggle-string-and-symbol)
+
+(defun jeg2s-toggle-ruby-block-type ()
+  "Toggle between brace and do/end block types."
+  (interactive)
+  (let ((regex (concat "\\`\\(?:"
+                       "{.*}"
+                       "\\|"
+                       "do\\b.+\\bend"
+                       "\\)\\'")))
+    (while (or (not (region-active-p))
+               (not (or (and (= (point-min) (region-beginning))
+                             (= (point-max) (region-end)))
+                        (string-match regex (buffer-substring-no-properties
+                                             (region-beginning)
+                                             (region-end))))))
+      (call-interactively 'er/expand-region))
+    (let ((matched (buffer-substring-no-properties (region-beginning)
+                                                   (region-end))))
+      (if (string-match regex matched)
+          (cond ((string= (substring matched 0 1) "{")
+                 (let ((lines (split-string
+                               (concat "do"
+                                       (if (= (count-matches "\n"
+                                                             (point)
+                                                             (mark)) 0)
+                                           (substring
+                                            (jeg2s-regex-replace
+                                             (jeg2s-regex-replace
+                                              matched
+                                              "\\`\\({\\(?:\s*|[^|]*|\\)?\\)\s*"
+                                              "\\1\n")
+                                             "\s*}\\'"
+                                             "\n}")
+                                            1
+                                            -1)
+                                         (substring matched 1 -1))
+                                       "end")
+                               "\n")))
+                   (call-interactively 'backward-delete-char-untabify)
+                   (loop for line in lines do
+                         (unless (string= line (car lines)) (newline))
+                         (insert line))
+                   (indent-for-tab-command)
+                   (previous-line)
+                   (unless (eolp) (move-end-of-line nil))
+                   (indent-for-tab-command)))
+                ((string= (substring matched 0 2) "do")
+                 ;; need to fix expand-region before this can be made to work
+                  ))))))
+(add-hook 'ruby-mode-hook
+          (lambda ()
+            (local-set-key (kbd "C-c t b") 'jeg2s-toggle-ruby-block-type)))
+
+(defun jeg2s-toggle-regex-type ()
+  "Toggle between standard and choose-your-own-quotes regex types."
+  (interactive)
+  (let ((regex (concat "\\`\\(?:"
+                       "/\\(?:\\\\\\\\\\|\\\\\.\\|[^/\\]+\\)*/"
+                       "\\|"
+                       "%r{.*}"
+                       "\\|"
+                       "{.*}"
+                       "\\)\\'")))
+    (while (or (not (region-active-p))
+               (not (or (and (= (point-min) (region-beginning))
+                             (= (point-max) (region-end)))
+                        (string-match regex (buffer-substring-no-properties
+                                             (region-beginning)
+                                             (region-end))))))
+      (call-interactively 'er/expand-region))
+    (let ((matched (buffer-substring-no-properties (region-beginning)
+                                                   (region-end))))
+      (if (string-match regex matched)
+          (cond ((string= (substring matched 0 1) "/")
+                 (call-interactively 'backward-delete-char-untabify)
+                 (let ((old_point (point)))
+                   (insert (concat "%r{"
+                                   (jeg2s-regex-replace-all
+                                    (substring matched 1 -1)
+                                    "\\\\/"
+                                    "/")
+                                   "}"))
+                   (goto-char (+ old_point 3))))
+                ((string= (substring matched 0 1) "{")
+                 (call-interactively 'backward-delete-char-untabify)
+                 (call-interactively 'backward-delete-char-untabify)
+                 (call-interactively 'backward-delete-char-untabify)
+                 (let ((old_point (point)))
+                   (insert (concat "/"
+                                   (jeg2s-regex-replace (substring matched 1 -1)
+                                                        "/"
+                                                        "\\\\/")
+                                   "/"))
+                   (goto-char (+ old_point 1))))
+                ((string= (substring matched 0 3) "%r{")
+                 (call-interactively 'backward-delete-char-untabify)
+                 (let ((old_point (point)))
+                   (insert (concat "/"
+                                   (jeg2s-regex-replace (substring matched 3 -1)
+                                                        "/"
+                                                        "\\\\/")
+                                   "/"))
+                   (goto-char (+ old_point 1)))))))))
+(global-set-key (kbd "C-c t r") 'jeg2s-toggle-regex-type)
+
+(defun jeg2s-toggle-ruby-hash-type ()
+  "Toggle between symbol key hash types."
+  (interactive)
+  (while (and (not
+               (looking-at
+                "\\(?::[A-Za-z0-9_]+\s*=>\\|[^A-Za-z0-9_][A-Za-z0-9_]+:\\)"))
+              (> (point) (point-min)))
+    (backward-char))
+  (if (looking-at ":[A-Za-z0-9_]+\s*=>")
+      (progn (delete-char 1)
+             (while (not (looking-at "\s*=>")) (forward-word))
+             (while (looking-at "\s") (delete-char 1))
+             (delete-char 2)
+             (insert ":")
+             (backward-char 2))
+    (forward-char)
+    (insert ":")
+    (let ((end_point (point)))
+      (while (not (looking-at ":")) (forward-word))
+      (delete-char 1)
+      (insert " =>")
+      (goto-char end_point))))
+(add-hook 'ruby-mode-hook
+          (lambda ()
+            (local-set-key (kbd "C-c t h") 'jeg2s-toggle-ruby-hash-type)))

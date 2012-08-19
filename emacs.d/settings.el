7a143f95814adb1f2ec5f5f36444ac06a9c9ea5e
James Edward Gray II <james@graysoftinc.com>
2012-08-19 13:42:19 -0500

    Fixing shift up.

--- emacs.d/settings.el
+++ emacs.d/settings.el
@@ -1,3 +1,10 @@
+;; fix S-<up> in xterm
+;; why:  http://lists.gnu.org/archive/html/help-gnu-emacs/2011-05/msg00211.html
+(if (equal "xterm" (tty-type))
+    (define-key input-decode-map "\e[1;2A" [S-up]))
+(defadvice terminal-init-xterm (after select-shift-up activate)
+  (define-key input-decode-map "\e[1;2A" [S-up]))
+
 ;; use spaces instead of tabs
 (setq-default indent-tabs-mode nil)
 (setq         tab-width        4)

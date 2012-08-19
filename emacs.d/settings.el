02c4ac05b0976253651d0f301ed77956970bb2b1
James Edward Gray II <james@graysoftinc.com>
2012-08-19 10:30:46 -0500

    Configuring TRAMP.

--- emacs.d/settings.el
+++ emacs.d/settings.el
@@ -92,3 +92,10 @@
 (require 're-builder)
 (setq reb-re-syntax 'string)
 (global-set-key (kbd "C-c o e") 're-builder)
+
+;; configure tramp
+(require 'tramp)
+(add-to-list 'tramp-default-proxies-alist
+             '(nil "\\`root\\'" "/ssh:%h:"))
+(add-to-list 'tramp-default-proxies-alist
+             '((regexp-quote (system-name)) nil nil))

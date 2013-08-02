02dfdd76fd2a563d093a53d69767bafacfc273ed
James Edward Gray II <james@graysoftinc.com>
2013-08-02 14:10:47 -0500

    Removing old Emacs config.

--- emacs.d/init.el
+++
@@ -1,9 +0,0 @@
-;; JEG2's Emacs configuration
-
-;; load configuration settings by type
-(load (expand-file-name "packages.el"    user-emacs-directory))  ;; load first
-(load (expand-file-name "secrets.el"     user-emacs-directory))  ;; not in Git
-(load (expand-file-name "functions.el"   user-emacs-directory))
-(load (expand-file-name "settings.el"    user-emacs-directory))
-(load (expand-file-name "appearance.el"  user-emacs-directory))
-(load (expand-file-name "keybindings.el" user-emacs-directory))  ;; load last

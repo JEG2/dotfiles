d50cf6f8d42e570b842e4e43737ac187953b0a5d
James Edward Gray II <james@graysoftinc.com>
2013-12-01 18:00:38 -0600

    Removing unused buffers.

--- emacs.d/jeg2/init/compile.el
+++ emacs.d/jeg2/init/compile.el
@@ -3,6 +3,9 @@
   (byte-recompile-directory (expand-file-name "jeg2" user-emacs-directory) 0)
   ;; don't compile init.el because it's too late to freshen it on load
   ;; loaddefs.el cannot be compiled
+  (let ((log (get-buffer "*Compile-Log*")))
+    (if (and log (= (buffer-size log) 0))
+        (kill-buffer "*Compile-Log*")))
   )
 
 (jeg2/byte-recompile-init)
---
+++ emacs.d/jeg2/settings/buffers.el
@@ -0,0 +1,2 @@
+;; shut off the scratch buffer (I use ielm when needed)
+(kill-buffer "*scratch*")

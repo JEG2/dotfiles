f9871e70e33cbf4792ad056fdf25433f6118418f
James Edward Gray II <james@graysoftinc.com>
2014-05-31 11:42:18 -0500

    Cleaning up some code.

--- emacs.d/jeg2/commands/scad.el
+++ emacs.d/jeg2/commands/scad.el
@@ -3,5 +3,6 @@
   "Preview the current buffer as a PNG image."
   (interactive)
   (save-buffer)
-  (call-process "openscad" nil nil nil "-o" (concat (file-name-sans-extension (buffer-file-name)) ".png") (buffer-file-name))
-  (call-process "open" nil nil nil (concat (file-name-sans-extension (buffer-file-name)) ".png")))
+  (let ((png (concat (file-name-sans-extension (buffer-file-name)) ".png")))
+    (if (= (call-process "openscad" nil nil nil "-o" png (buffer-file-name)) 0)
+        (call-process "open" nil nil nil png))))

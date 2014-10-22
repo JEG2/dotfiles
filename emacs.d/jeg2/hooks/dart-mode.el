8161ced5079b6d401aa9c5f19763e562778691bb
James Edward Gray II <james@graysoftinc.com>
2014-10-21 21:53:02 -0500

    Minimal dart-mode configuration.

---
+++ emacs.d/jeg2/hooks/dart-mode.el
@@ -0,0 +1,23 @@
+;; show wrap guide
+(require 'fill-column-indicator)
+(setq fci-rule-column jeg2-wrap-limit)
+(fci-mode)
+
+(local-set-key (kbd "C-c v w") 'fci-mode)
+
+;; show junk whitespace
+(whitespace-mode)
+
+;; activate character pairing
+(smartparens-mode)
+
+;; do some spell checking (requires:  brew install aspell --lang=en)
+(flyspell-prog-mode)
+
+(local-set-key (kbd "C-c v s") 'jeg2/toggle-program-spelling)
+
+;; command shortcuts
+(local-set-key (kbd "C-c =") 'jeg2/align=)
+(local-set-key (kbd "C-c e") 'jeg2/close-statement)
+
+(local-set-key (kbd "C-c t s") 'jeg2/toggle-string-type)

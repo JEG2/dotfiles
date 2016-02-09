a6b7acf268bad0e46769c4cf87757d088347ca1b
James Edward Gray II <james@graysoftinc.com>
2016-02-08 21:13:46 -0600

    Removing an old repo that I no longer need.

--- emacs.d/jeg2/init/packages.el
+++ emacs.d/jeg2/init/packages.el
@@ -1,6 +1,6 @@
 (package-initialize)
 (dolist (repo '(("elpa"      . "http://tromey.com/elpa/")
-                ("marmalade" . "http://marmalade-repo.org/packages/")
+;                ("marmalade" . "http://marmalade-repo.org/packages/")
                 ("melpa"     . "http://melpa.milkbox.net/packages/")))
   (add-to-list 'package-archives repo))
 

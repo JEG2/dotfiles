5e6e02a3a001f2c6338cb4d59e26b206c6908eee
James Edward Gray II <james@graysoftinc.com>
2018-12-11 13:22:28 -0600

    Adding streaming shortcuts.

---
+++ spacemacs.d/customizations/custom-frame-commands.el
@@ -0,0 +1,9 @@
+(defun jeg2/set-full-size ()
+  "Size frame for normal work."
+  (interactive)
+  (set-frame-size (selected-frame) 164 69))
+
+(defun jeg2/set-streaming-size ()
+  "Size frame for streaming."
+  (interactive)
+  (set-frame-size (selected-frame) 126 35))
